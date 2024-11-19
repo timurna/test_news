c133fc4d4f6e6fff9ad1c347db1b879056e54868
timurna <62594928+timurna@users.noreply.github.com>
2024-11-19 14:26:25 +0100

    Update py_streamlit_newsletter_04.py

--- py_streamlit_newsletter_04.py
+++ py_streamlit_newsletter_04.py
@@ -106,7 +106,7 @@ def download_and_load_data(file_url, data_version):
         return None
 
 # Ensure proper authentication
-if not st.session_state.authenticated:
+if not st.session_state['authenticated']:
     login()
 else:
     # User is authenticated
@@ -221,13 +221,212 @@ else:
         if st.session_state['run_clicked']:
             # **Perform data processing here after "Run" is clicked**
 
-            # [ ... existing code for data processing and metric calculations ... ]
-            # (For brevity, I'm not repeating all the existing code here.)
+            # Glossary content with metrics integrated
+            glossary = {
+                'Rating Metrics': '',
+                'Overall Rating': 'Player\'s overall performance across all metrics.',
+                'Defensive Rating': 'Player\'s overall defensive performance. Metrics: TcklMade%, TcklAtt, Tckl, AdjTckl, TcklA3, Blocks, Int, AdjInt, Clrnce',
+                'Goal Threat Rating': 'Player\'s threat to score goals. Metrics: Goal, Shot/Goal, MinPerGoal, ExpG, xGOT, xG +/- , Shot, SOG, Shot conversion, OnTarget%',
+                'Offensive Rating': 'Player\'s overall offensive performance. Metrics: 2ndAst, Ast, ExpG, ExpGExPn, Goal, GoalExPn, KeyPass, MinPerChnc, MinPerGoal, PsAtt, PsCmp, Pass%, PsIntoA3rd, PsRec, ProgCarry, ProgPass, Shot, Shot conversion, Shot/Goal, SOG, OnTarget%, Success1v1, Take on into the Box, TakeOn, ThrghBalls, TouchOpBox, Touches, xA, xA +/- , xG +/- , xGOT',
+                'Physical Offensive Rating': 'Player\'s physical contributions to offensive play. Metrics: PSV-99, Distance, M/min, HSR Distance, HSR Count, Sprint Distance, Sprint Count, HI Distance, HI Count, Medium Acceleration Count, High Acceleration Count, Medium Deceleration Count, High Deceleration Count',
+                'Physical Defensive Rating': 'Player\'s physical contributions to defensive play. Metrics: Distance OTIP, M/min OTIP, HSR Distance OTIP, HSR Count OTIP, Sprint Distance OTIP, Sprint Count OTIP, HI Distance OTIP, HI Count OTIP, Medium Acceleration Count OTIP, High Acceleration Count OTIP, Medium Deceleration Count OTIP, High Deceleration Count OTIP',
+                'Offensive Metrics': '',
+                '2ndAst': 'The pass that assists the assist leading to a goal.',
+                'Ast': 'Assists.',
+                'ExpG': 'Expected goals.',
+                'ExpGExPn': 'Expected goals excluding penalties.',
+                'Goal': 'Goals scored.',
+                'GoalExPn': 'Goals excluding penalties.',
+                'KeyPass': 'Passes that directly lead to a shot on goal.',
+                'MinPerChnc': 'Minutes per chance created.',
+                'MinPerGoal': 'Minutes per goal.',
+                'OnTarget%': 'Percentage of shots on target out of total shots.',
+                'PsAtt': 'Passes attempted.',
+                'PsCmp': 'Passes completed.',
+                'Pass%': 'Percentage of completed passes out of total passes attempted.',
+                'PsIntoA3rd': 'Passes into the attacking third.',
+                'PsRec': 'Passes received by the player.',
+                'ProgCarry': 'Progressive carries, advancing the ball significantly.',
+                'ProgPass': 'Progressive passes, advancing the ball significantly.',
+                'Shot': 'Total shots taken.',
+                'Shot conversion': 'Shots on target per goal.',
+                'Shot/Goal': 'Total shots per goal.',
+                'SOG': 'Shots on goal.',
+                'Success1v1': 'Successful dribbles against an opponent.',
+                'Take on into the Box': 'Number of successful dribbles into the penalty box.',
+                'TakeOn': 'Attempted dribbles against an opponent.',
+                'ThrghBalls': 'Through balls played.',
+                'TouchOpBox': 'Number of touches in the opponent\'s penalty box.',
+                'Touches': 'Total number of touches.',
+                'xA': 'Expected assists.',
+                'xA +/-': 'Expected assists compared to actual assists.',
+                'xG +/-': 'Expected goals compared to actual goals.',
+                'xGOT': 'Expected goals on target.',
+                'Defensive Metrics': '',
+                'AdjInt': 'Adjusted interceptions, considering context.',
+                'AdjTckl': 'Adjusted tackles, considering context.',
+                'Blocks': 'Total blocks made.',
+                'Clrnce': 'Clearances made.',
+                'Int': 'Interceptions made.',
+                'Tckl': 'Tackles made.',
+                'TcklMade%': 'Percentage of tackles successfully made out of total tackle attempts.',
+                'TcklA3': 'Tackles made in the attacking third.',
+                'TcklAtt': 'Tackles attempted.',
+                'Physical Metrics': '',
+                'PSV-99': 'Peak Sprint Velocity (Maximum Speed).',
+                'Distance': 'Total distance covered by the player during the match.',
+                'Distance OTIP': 'Distance covered while opponent has ball possession (OTIP).',
+                'HI Count': 'High-intensity actions performed.',
+                'HI Count OTIP': 'High-intensity actions performed while opponent has ball possession (OTIP).',
+                'HI Distance': 'High-intensity distance covered.',
+                'HI Distance OTIP': 'High-intensity distance covered while opponent has ball possession (OTIP).',
+                'High Acceleration Count': 'High-intensity accelerations performed.',
+                'High Acceleration Count OTIP': 'High-intensity accelerations performed while opponent has ball possession (OTIP).',
+                'High Deceleration Count': 'High-intensity decelerations performed.',
+                'High Deceleration Count OTIP': 'High-intensity decelerations performed while opponent has ball possession (OTIP).',
+                'HSR Count': 'Count of high-speed running actions.',
+                'HSR Count OTIP': 'High-speed running actions performed while opponent has ball possession (OTIP).',
+                'HSR Distance': 'High-speed running distance covered.',
+                'HSR Distance OTIP': 'High-speed running distance covered while opponent has ball possession (OTIP).',
+                'M/min': 'Meters covered per minute by the player.',
+                'M/min OTIP': 'Meters per minute covered while opponent has ball possession (OTIP).',
+                'Medium Acceleration Count': 'Medium-intensity accelerations performed.',
+                'Medium Acceleration Count OTIP': 'Medium-intensity accelerations performed while opponent has ball possession (OTIP).',
+                'Medium Deceleration Count': 'Medium-intensity decelerations performed.',
+                'Medium Deceleration Count OTIP': 'Medium-intensity decelerations performed while opponent has ball possession (OTIP).',
+                'Sprint Count': 'Total sprints performed.',
+                'Sprint Count OTIP': 'Sprint actions performed while opponent has ball possession (OTIP).',
+                'Sprint Distance': 'Total distance covered while sprinting.',
+                'Sprint Distance OTIP': 'Sprint distance covered while opponent has ball possession (OTIP).'
+            }
+
+            # Calculate age from birthdate
+            data['DOB'] = pd.to_datetime(data['DOB'])
+            today = datetime.today()
+            data['Age'] = data['DOB'].apply(lambda x: today.year - x.year - ((today.month, today.day) < (x.month, x.day)))
+
+            # Ensure 'Date' is in datetime format
+            data['Date'] = pd.to_datetime(data['Date'])
+
+            # Convert text-based numbers to numeric, handling percentage metrics
+            percentage_metrics = ['TcklMade%', 'Pass%', 'OnTarget%']
+
+            # Remove percentage signs and convert to numeric
+            for metric in percentage_metrics:
+                if metric in data.columns:
+                    data[metric] = pd.to_numeric(data[metric].astype(str).str.replace('%', ''), errors='coerce')
+
+            # Convert other text-based numbers to numeric
+            physical_metrics = ['PSV-99', 'Distance', 'M/min', 'HSR Distance', 'HSR Count', 'Sprint Distance',
+                                'Sprint Count', 'HI Distance', 'HI Count', 'Medium Acceleration Count',
+                                'High Acceleration Count', 'Medium Deceleration Count', 'High Deceleration Count',
+                                'Distance OTIP', 'M/min OTIP', 'HSR Distance OTIP', 'HSR Count OTIP',
+                                'Sprint Distance OTIP', 'Sprint Count OTIP', 'HI Distance OTIP', 'HI Count OTIP',
+                                'Medium Acceleration Count OTIP', 'High Acceleration Count OTIP',
+                                'Medium Deceleration Count OTIP', 'High Deceleration Count OTIP']
+
+            offensive_metrics = [
+                '2ndAst', 'Ast', 'ExpG', 'ExpGExPn', 'Goal', 'GoalExPn', 'KeyPass',
+                'MinPerChnc', 'MinPerGoal', 'PsAtt', 'PsCmp', 'Pass%', 'PsIntoA3rd',
+                'PsRec', 'ProgCarry', 'ProgPass', 'Shot', 'Shot conversion',
+                'Shot/Goal', 'SOG', 'OnTarget%', 'Success1v1', 'Take on into the Box',
+                'TakeOn', 'ThrghBalls', 'TouchOpBox', 'Touches', 'xA',
+                'xA +/-', 'xG +/-', 'xGOT'
+            ]
+
+            defensive_metrics = [
+                'TcklMade%', 'TcklAtt', 'Tckl', 'AdjTckl', 'TcklA3', 'Blocks', 'Int', 'AdjInt', 'Clrnce'
+            ]
+
+            goal_threat_metrics = [
+                'Goal', 'Shot/Goal', 'MinPerGoal', 'ExpG', 'xGOT', 'xG +/-',
+                'Shot', 'SOG', 'Shot conversion', 'OnTarget%'
+            ]
+
+            # Combine all metrics for processing
+            all_metrics = list(set(
+                physical_metrics + offensive_metrics + defensive_metrics + goal_threat_metrics + percentage_metrics
+            ))
+
+            for metric in all_metrics:
+                if metric in data.columns and metric not in percentage_metrics:  # Exclude percentage metrics already processed
+                    data[metric] = pd.to_numeric(data[metric].astype(str).str.replace(',', '.'), errors='coerce')
+
+            # Fill NaN values with 0 only for players who have any non-NaN value in the group of metrics
+            def fill_na_conditionally(df, metric_group):
+                # Create a mask where any metric in the group is not NaN
+                mask = df[metric_group].notna().any(axis=1)
+                # Apply filling only to rows where the mask is True
+                df.loc[mask, metric_group] = df.loc[mask, metric_group].fillna(0)
+
+            fill_na_conditionally(data, physical_metrics)
+            fill_na_conditionally(data, offensive_metrics)
+            fill_na_conditionally(data, defensive_metrics)
+            fill_na_conditionally(data, goal_threat_metrics)
+
+            # Initialize the scalers
+            scaler = MinMaxScaler(feature_range=(0, 10))
+            quantile_transformer = QuantileTransformer(output_distribution='uniform', random_state=0)
+
+            # Define physical metrics subsets
+            physical_offensive_metrics = [
+                'PSV-99', 'Distance', 'M/min', 'HSR Distance', 'HSR Count', 'Sprint Distance',
+                'Sprint Count', 'HI Distance', 'HI Count',
+                'Medium Acceleration Count', 'High Acceleration Count',
+                'Medium Deceleration Count', 'High Deceleration Count'
+            ]
+
+            physical_defensive_metrics = [
+                'Distance OTIP', 'M/min OTIP', 'HSR Distance OTIP', 'HSR Count OTIP',
+                'Sprint Distance OTIP', 'Sprint Count OTIP', 'HI Distance OTIP',
+                'HI Count OTIP', 'Medium Acceleration Count OTIP',
+                'High Acceleration Count OTIP', 'Medium Deceleration Count OTIP',
+                'High Deceleration Count OTIP'
+            ]
+
+            # Calculate the ratings
+            data['Physical Offensive Rating'] = scaler.fit_transform(
+                quantile_transformer.fit_transform(data[physical_offensive_metrics].fillna(0))
+            ).mean(axis=1)
+
+            data['Physical Defensive Rating'] = scaler.fit_transform(
+                quantile_transformer.fit_transform(data[physical_defensive_metrics].fillna(0))
+            ).mean(axis=1)
+
+            data['Offensive Rating'] = scaler.fit_transform(
+                quantile_transformer.fit_transform(data[offensive_metrics].fillna(0))
+            ).mean(axis=1)
+
+            data['Defensive Rating'] = scaler.fit_transform(
+                quantile_transformer.fit_transform(data[defensive_metrics].fillna(0))
+            ).mean(axis=1)
+
+            data['Goal Threat Rating'] = scaler.fit_transform(
+                quantile_transformer.fit_transform(data[goal_threat_metrics].fillna(0))
+            ).mean(axis=1)
+
+            # **Add the Overall Rating by combining all metrics**
+            # Create a list of all metrics used in the ratings
+            rating_metrics = ['Overall Rating', 'Physical Offensive Rating', 'Physical Defensive Rating',
+                              'Offensive Rating', 'Defensive Rating', 'Goal Threat Rating']
+
+            data['Overall Rating'] = data[['Physical Offensive Rating', 'Physical Defensive Rating',
+                                           'Offensive Rating', 'Defensive Rating', 'Goal Threat Rating']].mean(axis=1)
+
+            # **Calculate Cumulative Averages for Metrics**
 
             # Ensure the data is sorted
             data = data.sort_values(['League', 'playerFullName', 'Date'])
 
-            # [ ... code to calculate cumulative averages ... ]
+            # Create a list of metrics for which we want cumulative averages
+            metrics_for_cum_avg = rating_metrics + physical_offensive_metrics + physical_defensive_metrics + offensive_metrics + defensive_metrics
+
+            # Remove duplicates
+            metrics_for_cum_avg = list(set(metrics_for_cum_avg))
+
+            # Calculate cumulative averages for each player in each league
+            for metric in metrics_for_cum_avg:
+                data[f'{metric}_cum_avg'] = data.groupby(['League', 'playerFullName'])[metric].expanding().mean().reset_index(level=[0, 1], drop=True)
 
             # **Updated Filtering:**
             # Filter the data by the selected position group and the selected matchdays
@@ -453,7 +652,35 @@ else:
 
             # Glossary section - Render only after authentication inside an expander
             with st.expander("Glossary"):
-                # [ ... existing code for glossary ... ]
+                sections = {
+                    "Rating Metrics": [
+                        'Overall Rating', 'Defensive Rating', 'Goal Threat Rating', 'Offensive Rating',
+                        'Physical Defensive Rating', 'Physical Offensive Rating'
+                    ],
+                    "Offensive Metrics": [
+                        '2ndAst', 'Ast', 'ExpG', 'ExpGExPn', 'Goal', 'GoalExPn', 'KeyPass',
+                        'MinPerChnc', 'MinPerGoal', 'PsAtt', 'PsCmp', 'Pass%', 'PsIntoA3rd',
+                        'PsRec', 'ProgCarry', 'ProgPass', 'Shot', 'OnTarget%', 'Shot conversion',
+                        'Shot/Goal', 'SOG', 'Success1v1', 'Take on into the Box',
+                        'TakeOn', 'ThrghBalls', 'TouchOpBox', 'Touches', 'xA',
+                        'xA +/-', 'xG +/-', 'xGOT'
+                    ],
+                    "Defensive Metrics": [
+                        'AdjInt', 'AdjTckl', 'Blocks', 'Clrnce', 'Int',
+                        'TcklAtt', 'Tckl', 'TcklMade%', 'TcklA3'
+                    ],
+                    "Physical Offensive Metrics": physical_offensive_metrics,
+                    "Physical Defensive Metrics": physical_defensive_metrics
+                }
+
+                # Iterate over each section
+                for section, metrics in sections.items():
+                    st.markdown(f"<h3 style='font-size:15px; color:#333; font-weight:bold;'>{section}</h3>", unsafe_allow_html=True)
+                    # Iterate over the metrics for the current section
+                    for metric in metrics:
+                        # Display the metric and its explanation in italic
+                        explanation = glossary.get(metric, "")
+                        st.markdown(f"{metric}: *{explanation}*")
 
         else:
             st.write("Please set your filters and click 'Run' to display the data.")
