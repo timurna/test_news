febb039923fba79fed8ebec34a394320e96d1603
timurna <62594928+timurna@users.noreply.github.com>
2024-12-10 17:02:15 -0500

    Update py_streamlit_newsletter_04.py

--- py_streamlit_newsletter_04.py
+++ py_streamlit_newsletter_04.py
@@ -87,7 +87,7 @@ def set_mobile_css():
 @st.cache_data
 def download_and_load_data(file_url, data_version):
     # Define the file path for the downloaded parquet file
-    parquet_file = f'/tmp/newup1_{data_version}.parquet'
+    parquet_file = f'/tmp/newup3_{data_version}.parquet'
 
     # Download the file using gdown with fuzzy=True
     try:
@@ -114,7 +114,7 @@ else:
     st.write("Welcome! You are logged in.")
 
     # Load the dataset **only** after successful login
-    file_url = 'https://drive.google.com/uc?id=1kvnOz-wzAOKf8IxqV-vZIjiQtZ5IFsi0'
+    file_url = 'https://drive.google.com/uc?id=1iBCnl-ajQiJDS1-kMEECz_t5t5hrMNyE'
     data_version = 'v3'  # Update this to a new value when your data changes
     data = download_and_load_data(file_url, data_version)
 
